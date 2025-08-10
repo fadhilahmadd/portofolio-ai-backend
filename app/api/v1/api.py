ff089f03065f0553b13df811d30e3ff92bdcc9d2
fadhilahmadd <zashxz011@gmail.com>
2025-08-10 13:36:26 +0700

    clean the code

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import chat, resume
+from app.api.v1.endpoints import chat
 
 api_router = APIRouter()
 
 api_router.include_router(chat.router, prefix="/chat", tags=["chatbot"])
-api_router.include_router(resume.router, prefix="/resume", tags=["Resume"])
